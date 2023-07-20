fec573627fc6ebf6b2ba4394e75265bb80d92443
Angelica Turbela <angelcturbela@gmail.com>
2023-07-20 16:58:47 +0200

    Fixed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// window.addEventListener('load', startGame)
 //Globals
 let wordToCheck = [];
-let points = 0; // will be used to keep track of the player's score
+let points = 0;
 let lifeCount = 3;
 let startTimeStamp;
 let timerInterval;
@@ -10,13 +9,10 @@ let theWord = "";
 let theNextWord = "";
 let letterIndex = 0;
 
-//======$$$$====DOM====$$$$======//
+// DOM elements
 const mainScreen = document.getElementById("main-screen");
 const gameScreen = document.getElementById("game-screen");
 const gameOver = document.getElementById("game-over-screen");
-// const heartOne = document.getElementById("heart1");
-// const heartTwo = document.getElementById("heart2");
-// const heartThree= document.getElementById("heart3");
 const angelInGame = document.querySelector(".angelGameCharacter");
 let startBtn = document.querySelector(".btn-play");
 let reStartBtn = document.querySelector(".btn-replay");
@@ -27,386 +23,321 @@ let life = document.querySelector(".life");
 let endScore = document.querySelector(".scoreDisplay");
 let timeDuration = document.querySelector(".timer");
 let endTime = document.querySelector(".playDuration");
-// const heartsContainer = document.querySelector(".hearts-container");
-// const hearts = heartsContainer.querySelectorAll(".heart");
 const heartsContainer = document.querySelector(".hearts-container");
 const hearts = heartsContainer.querySelectorAll(".heart:not(.hidden)");
 
 const list = [
-  "SAMPLE",
-  "RANDOM",
-  "WORDS",
-  "SAMPLE",
-  "RANDOM",
-  "WORDS",
-  "SAMPLE",
-  "RANDOM",
-  "WORDS",
-  "SAMPLE",
-  "RANDOM",
-  "WORDS",
-  "QUITE",
-  "RABBIT",
-  "RACE",
-  "RADIO",
-  "RAILROAD",
-  "RAIN",
-  "RAISE",
-  "RAN",
-  "RANCH",
-  "RANGE",
-  "RAPIDLY",
-  "RATE",
-  "RATHER",
-  "RAW",
-  "RAYS",
-  "REACH",
-  "READ",
-  "READER",
-  "READY",
-  "REAL",
-  "REALIZE",
-  "REAR",
-  "REASON",
-  "RECALL",
-  "RECEIVE",
-  "RECENT",
-  "RECENTLY",
-  "RECOGNIZE",
-  "RECORD",
-  "RED",
-  "REFER",
-  "REFUSED",
-  "REGION",
-  "REGULAR",
-  "RELATED",
-  "RELATIONSHIP",
-  "RELIGIOUS",
-  "REMAIN",
-  "REMARKABLE",
-  "REMEMBER",
-  "REMOVE",
-  "REPEAT",
-  "REPLACE",
-  "REPLIED",
-  "REPORT",
-  "REPRESENT",
-  "REQUIRE",
-  "RESEARCH",
-  "RESPECT",
-  "REST",
-  "RESULT",
-  "RETURN",
-  "REVIEW",
-  "RHYME",
-  "RHYTHM",
-  "RICE",
-  "RICH",
-  "RIDE",
-  "RIDING",
-  "RIGHT",
-  "RING",
-  "RISE",
-  "RISING",
-  "RIVER",
-  "ROAD",
-  "ROAR",
-  "ROCK",
-  "ROCKET",
-  "ROCKY",
-  "ROD",
-  "ROLL",
-  "ROOF",
-  "ROOM",
-  "ROOT",
-  "ROPE",
-  "ROSE",
-  "ROUGH",
-  "ROUND",
-  "ROUTE",
-  "ROW",
-  "RUBBED",
-  "RUBBER",
-  "RULE",
-  "RULER",
-  "RUN",
-  "RUNNING",
-  "RUSH",
-  "SAD",
-  "SADDLE",
-  "SAFE",
-  "SAFETY",
-  "SAID",
-  "SAIL",
-  "SALE",
-  "SALMON",
-  "SALT",
-  "SAME",
-  "SAND",
-  "SANG",
-  "SAT",
-  "SATELLITES",
-  "SATISFIED",
-  "SAVE",
-  "SAVED",
-  "SAW",
-  "SAY",
-  "SCALE",
-  "SCARED",
-  "SCENE",
-  "SCHOOL",
-  "SCIENCE",
-  "SCIENTIFIC",
-  "SCIENTIST",
-  "SCORE",
-  "SCREEN",
-  "SEA",
-  "SEARCH",
-  "SEASON",
-  "SEAT",
-  "SECOND",
-  "SECRET",
-  "SECTION",
-  "SEE",
-  "SEED",
-  "SEEING",
+    "SAMPLE",
+    "RANDOM",
+    "WORDS",
+    "SAMPLE",
+    "RANDOM",
+    "WORDS",
+    "SAMPLE",
+    "RANDOM",
+    "WORDS",
+    "SAMPLE",
+    "RANDOM",
+    "WORDS",
+    "QUITE",
+    "RABBIT",
+    "RACE",
+    "RADIO",
+    "RAILROAD",
+    "RAIN",
+    "RAISE",
+    "RAN",
+    "RANCH",
+    "RANGE",
+    "RAPIDLY",
+    "RATE",
+    "RATHER",
+    "RAW",
+    "RAYS",
+    "REACH",
+    "READ",
+    "READER",
+    "READY",
+    "REAL",
+    "REALIZE",
+    "REAR",
+    "REASON",
+    "RECALL",
+    "RECEIVE",
+    "RECENT",
+    "RECENTLY",
+    "RECOGNIZE",
+    "RECORD",
+    "RED",
+    "REFER",
+    "REFUSED",
+    "REGION",
+    "REGULAR",
+    "RELATED",
+    "RELATIONSHIP",
+    "RELIGIOUS",
+    "REMAIN",
+    "REMARKABLE",
+    "REMEMBER",
+    "REMOVE",
+    "REPEAT",
+    "REPLACE",
+    "REPLIED",
+    "REPORT",
+    "REPRESENT",
+    "REQUIRE",
+    "RESEARCH",
+    "RESPECT",
+    "REST",
+    "RESULT",
+    "RETURN",
+    "REVIEW",
+    "RHYME",
+    "RHYTHM",
+    "RICE",
+    "RICH",
+    "RIDE",
+    "RIDING",
+    "RIGHT",
+    "RING",
+    "RISE",
+    "RISING",
+    "RIVER",
+    "ROAD",
+    "ROAR",
+    "ROCK",
+    "ROCKET",
+    "ROCKY",
+    "ROD",
+    "ROLL",
+    "ROOF",
+    "ROOM",
+    "ROOT",
+    "ROPE",
+    "ROSE",
+    "ROUGH",
+    "ROUND",
+    "ROUTE",
+    "ROW",
+    "RUBBED",
+    "RUBBER",
+    "RULE",
+    "RULER",
+    "RUN",
+    "RUNNING",
+    "RUSH",
+    "SAD",
+    "SADDLE",
+    "SAFE",
+    "SAFETY",
+    "SAID",
+    "SAIL",
+    "SALE",
+    "SALMON",
+    "SALT",
+    "SAME",
+    "SAND",
+    "SANG",
+    "SAT",
+    "SATELLITES",
+    "SATISFIED",
+    "SAVE",
+    "SAVED",
+    "SAW",
+    "SAY",
+    "SCALE",
+    "SCARED",
+    "SCENE",
+    "SCHOOL",
+    "SCIENCE",
+    "SCIENTIFIC",
+    "SCIENTIST",
+    "SCORE",
+    "SCREEN",
+    "SEA",
+    "SEARCH",
+    "SEASON",
+    "SEAT",
+    "SECOND",
+    "SECRET",
+    "SECTION",
+    "SEE",
+    "SEED",
+    "SEEING",
 ];
 
-//=====$$$$$=====RESET THE VARIABLES=======$$$$$=============
+// Function to initialize the game variables
 const initGame = () => {
-  spans = [];
-  startTimeStamp;
-  timerInterval;
-  gameStarted = true;
-  theWord = "";
-  theNextWord = "";
-  letterIndex = 0;
-  score.innerHTML = 0;
-  currentWordContainer.innerHTML = "";
-  nextWordContainer.innerHTML = "";
+    spans = [];
+    startTimeStamp;
+    timerInterval;
+    gameStarted = true;
+    theWord = "";
+    theNextWord = "";
+    letterIndex = 0;
+    score.innerHTML = 0;
+    currentWordContainer.innerHTML = "";
+    nextWordContainer.innerHTML = "";
 };
 
-// $$$$=====Show the main screen=====$$$$//
+// Function to show the main screen
 const showMainScreen = () => {
-  mainScreen.classList.remove("hidden");
-  gameScreen.classList.add("hidden");
-  gameOver.classList.add("hidden");
+    mainScreen.classList.remove("hidden");
+    gameScreen.classList.add("hidden");
+    gameOver.classList.add("hidden");
 };
 
-// $$$$=====Show the game screen=====$$$$//
+// Function to show the game screen
 const showGameScreen = () => {
-  mainScreen.classList.add("hidden");
-  gameScreen.classList.remove("hidden");
-  gameOver.classList.add("hidden");
+    mainScreen.classList.add("hidden");
+    gameScreen.classList.remove("hidden");
+    gameOver.classList.add("hidden");
 };
 
-// $$$$=====Show the game over screen=====$$$$//
+// Function to show the game over screen
 const showGameOver = () => {
-  mainScreen.classList.add("hidden");
-  gameScreen.classList.add("hidden");
-  gameOver.classList.remove("hidden");
+    mainScreen.classList.add("hidden");
+    gameScreen.classList.add("hidden");
+    gameOver.classList.remove("hidden");
 };
 
-// ===$$$===TIMER===$$$====
+// Function to update the game timer display
 const updateTimer = () => {
-  const currentTime = Math.floor((Date.now() - startTimeStamp) / 1000);
-  const minutes = Math.floor(currentTime / 60);
-  const seconds = currentTime % 60;
-  timeDuration.textContent = `${minutes}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
+    const currentTime = Math.floor((Date.now() - startTimeStamp) / 1000);
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = currentTime % 60;
+    timeDuration.textContent = `${minutes}:${seconds
+        .toString()
+        .padStart(2, "0")}`;
 };
+
+// Function to display the final game duration
 const endGameTime = () => {
-  const currentTime = Math.floor((Date.now() - startTimeStamp) / 1000);
-  endTime.textContent = currentTime;
-  console.log(currentTime);
+    const currentTime = Math.floor((Date.now() - startTimeStamp) / 1000);
+    endTime.textContent = currentTime;
 };
+
+// Function to start the timer
 const startTimer = () => {
-  startTimeStamp = Date.now();
-  timerInterval = setInterval(updateTimer, 1000);
+    startTimeStamp = Date.now();
+    timerInterval = setInterval(updateTimer, 1000);
 };
 
+// Function to stop the timer
 const stopTimer = () => {
-  clearInterval(timerInterval);
+    clearInterval(timerInterval);
 };
-// ===$$$$======START GAME=====$$$$======
+
+// Function to start the game
 const startGame = () => {
-  lifeCount = 3;
-  initGame();
-  showGameScreen();
-  endGameTime();
-  startTimer();
-  timerInterval = setInterval(updateTimer, 1000);
+    lifeCount = 3;
+    initGame();
+    showGameScreen();
+    endGameTime();
+    startTimer();
+    timerInterval = setInterval(updateTimer, 1000);
 };
 
-// ==$$=======GAME OVER =====$$$=========
+// Function to handle game over logic
 const gameOverLogic = () => {
-  gameStarted = false;
-  showGameOver();
-  stopTimer();
-  endGameTime();
-  
-  // const scoreDisplay = document.querySelector('.words-count');
-  // console.log(points);
-  endScore.textContent = `GAME OVER! You typed ${points} word(s) `;
-  endTime.textContent = `in ${endTime.textContent}seconds`;
+    gameStarted = false;
+    showGameOver();
+    stopTimer();
+    endGameTime();
+    endScore.textContent = `GAME OVER! You typed ${points} word(s) `;
+    endTime.textContent = `in ${endTime.textContent}seconds`;
 };
-// const nextRandom = () => {
-//   nextWord.innerHTML = ""; // clears previous word;
-//   const currentNextWordIndex = Math.floor(Math.random() * list.length); //generate random array index;
-//   theNextWord = list[currentNextWordIndex];
-//   const nextWordArray = theNextWord.split("");
-//   createSpans(nextWordArray, nextWord);
-// };
+
+// Function to generate a random word from the list
 const random = (container, theWord) => {
-  letterIndex = 0;
-  container.innerHTML = ""; // clears previous word;
-  const currentWordIndex = Math.floor(Math.random() * list.length); //generate random array index
-  theWord = list[currentWordIndex];
-  const wordArray = theWord.split("");
+    letterIndex = 0;
+    container.innerHTML = ""; // clears previous word;
+    const currentWordIndex = Math.floor(Math.random() * list.length); //generate random array index
+    theWord = list[currentWordIndex];
+    const wordArray = theWord.split("");
 
-  createSpans(wordArray, container);
+    createSpans(wordArray, container);
 };
 
+// Function to create spans for each letter of the word
 function createSpans(word, parent) {
-  for (let i = 0; i < word.length; i++) {
-    console.log(word[i]);
-    const span = document.createElement("span");
-    span.classList.add("span");
-    span.textContent = word[i];
-    parent.appendChild(span);
-  }
+    for (let i = 0; i < word.length; i++) {
+        const span = document.createElement("span");
+        span.classList.add("span");
+        span.textContent = word[i];
+        parent.appendChild(span);
+    }
 }
 
-
-// let bottom = 0;
-// let gravity = 0.12;
-// let isJumping = false;
-// function jumpChar() {
-//   if (isJumping) return;
-//   let jumpUpTimerId = setInterval(() => {
-//     if (bottom > 300) {
-//       clearInterval(jumpUpTimerId);
-//       let downTimerId = setInterval(() => {
-//         if (bottom < 0) {
-//           clearInterval(downTimerId);
-//           isJumping = false;
-//         }
-//         bottom -= 5;
-//         angelInGame.style.bottom = bottom + "px";
-//       }, 20);
-//     }
-//     isJumping = true;
-//     bottom += 30;
-//     bottom = bottom + gravity;
-//     console.log(bottom);
-//     angelInGame.style.bottom = bottom + "px";
-//   }, 10);
-// }
-
-//=====$$$$$======Checking every typed letters======$$$$$=======//
+// Function to handle typing letters during the game
 const typeLetters = (e) => {
-  if (!gameStarted) return;
-  let typed = e.key.toUpperCase();
-  let currentSpan = wordToCheck[letterIndex];
+    if (!gameStarted) return;
 
-  if (currentSpan.textContent === typed) {
-    currentSpan.classList.add("bg");
-    letterIndex++;
-    if (letterIndex === wordToCheck.length) {
-      //if word has been typed fully, switch
-      currentWordContainer.innerHTML = "";
+    let typed = e.key.toUpperCase();
+    let currentSpan = wordToCheck[letterIndex];
 
-      wordToCheck = document.querySelectorAll(".nextWords span");
-      currentWordContainer.append(...wordToCheck);
-      theWord = theNextWord;
-      random(nextWordContainer, theNextWord);
-      points += 1;
-      score.textContent = points;
-      console.log(points);
-      angelInGame.classList.add('jumpAnimation')
-      setTimeout(() => angelInGame.classList.remove("jumpAnimation"), 1000);
-      //jumpChar();
-      letterIndex = 0;
-      //   wordToCheck = [...spansTwo];
-      // random();
-      // nextRandom()
-      // setTimeout(random, 20)
-      // setTimeout(random, 30)
+    if (currentSpan.textContent === typed) {
+        currentSpan.classList.add("bg");
+        letterIndex++;
+        if (letterIndex === wordToCheck.length) {
+            //if word has been typed fully, switch
+            currentWordContainer.innerHTML = "";
+            wordToCheck = document.querySelectorAll(".nextWords span");
+            currentWordContainer.append(...wordToCheck);
+            theWord = theNextWord;
+            random(nextWordContainer, theNextWord);
+            points += 1;
+            score.textContent = points;
+
+            // Perform jump animation when word is typed correctly
+            angelInGame.classList.add('jumpAnimation')
+            setTimeout(() => angelInGame.classList.remove("jumpAnimation"), 2000);
+            //jumpChar();
+            letterIndex = 0;
+        }
+    } else {
+        lifeCount -= 1;
+        currentSpan.classList.add("error");
+        setTimeout(() => currentSpan.classList.remove("error"), 200);
+        updateHearts(lifeCount);
+        hearts[0].classList.add("hidden")
     }
-  } else {
-    lifeCount -= 1;
-    console.log(lifeCount);
-    currentSpan.classList.add("error");
-    setTimeout(() => currentSpan.classList.remove("error"), 200);
-    hearts[0].classList.add("hidden")
-    console.log("decrement the heart");
-    
-  }
-  //   life.textContent = lifeCount;
-  if (lifeCount <= 0) {
-    gameOverLogic();
-  }
 
-  //   for (let i=1; i <= 3; i++) {
-  //     const heart = document.querySelectorAll(`hearts${i}`);
-  //     if (i > lifeCount) {
-  //         heart.style.display = 'none';
-  //     } else {
-  //         heart.style.display = 'inline-block'
-  //     }
-  //   }
+    if (lifeCount <= 0) {
+        gameOverLogic();
+    }
 };
-// const updateHearts = (lifeCount) => {
-// //   const heartsContainer = document.querySelector(".hearts-container");
-// //   const hearts = heartsContainer.querySelectorAll(".heart");
-//   hearts[0].classList.add("hidden");
-// //   if (gameStarted) {
-// //     hearts[0].classList.remove("hidden")
-// // //   }
-// //   for (let i = 3; i > 0; i--) {
-// //       hearts[0].classList.add("hidden");
-// //   }
-// }
 
-// const removeHearts = () => {
-//   if (lifeCount === 3) {
-//     heartThree.classList.remove("hidden")
-//     heartTwo.classList.remove("hidden")
-//     heartOne.classList.remove("hidden")
-//   }
-//    if (lifeCount === 2) {
-//     heartThree.classList.add("hidden")
-//     heartTwo.classList.remove("hidden")
-//     heartOne.classList.remove("hidden")
-//   }
-//   if (lifeCount === 1) {
-//     heartThree.classList.add("hidden")
-//     heartTwo.classList.add("hidden")
-//     heartOne.classList.remove("hidden")
-//   }
-//   else {
-//     heartThree.classList.add("hidden")
-//     heartTwo.classList.add("hidden")
-//     heartOne.classList.add("hidden")
-//   }
-// }
-//====$$ CLICK BUTTON $$==== //
+// Function to update the hearts display based on the remaining life count
+const updateHearts = (lifeCount) => {
+    const heartsContainer = document.querySelector(".hearts-container");
+    const hearts = heartsContainer.querySelectorAll(".heart");
 
+    hearts.forEach((heart, index) => {
+        if (index < lifeCount) {
+            heart.classList.remove("hidden");
+        } else {
+            heart.classList.add("hidden");
+        }
+    });
+};
+
+// Event listener for keydown event to handle typing letters
 document.addEventListener("keydown", typeLetters);
 
+// Event listener for the "Start" button
 startBtn.addEventListener("click", function (e) {
-  startGame();
-  random(currentWordContainer, theWord);
-  random(nextWordContainer, theNextWord);
-  wordToCheck = document.querySelectorAll(".words span");
-  console.log(wordToCheck, nextWordContainer, currentWordContainer);
-  gameStarted = true;
+    startGame();
+    random(currentWordContainer, theWord);
+    random(nextWordContainer, theNextWord);
+    wordToCheck = document.querySelectorAll(".words span");
+    gameStarted = true;
 });
 
+// Event listener for the "Restart" button
 reStartBtn.addEventListener("click", function (e) {
-  startGame();
-  random(currentWordContainer, theWord);
-  random(nextWordContainer, theNextWord);
-  gameStarted = true;
+    startGame();
+    random(currentWordContainer, theWord);
+    random(nextWordContainer, theNextWord);
+    gameStarted = true;
 });
